299bfec21a4dec9a88b18117d279e1036e770bbe
BrianTruong23 <truongthoithang@gmail.com>
2023-01-06 22:41:54 -0800

    change meet the team

--- js/base/footer.js
+++ js/base/footer.js
@@ -1,36 +1,36 @@
 document.write(`
+
 <footer>
-    <div class="content">
-      <div class="left box center">
+<div class="content">
+  <div class="left box center">
 
-          <!-- <img src="image/logo-vt.png" alt=""> -->
-          <div class="topic">Learn more about us</div>
-          <div><a href="#">Meet The Team</a></div>
-          <div><a href="#">Student Application</a></div>
-          <div><a href="#">Instructors Application</a></div>
-          <div><a href="#">FAQ</a></div>
+      <!-- <img src="image/logo-vt.png" alt=""> -->
+      <div class="topic">Learn more about us</div>
+      <div><a href="meet_team.html">Meet The Team</a></div>
+      <div><a href="https://docs.google.com/forms/d/e/1FAIpQLSf9twKPOzJU1NK_zaj7VbFvAzYDS_tzHAL1Y-LxA3KlXpgZ1w/viewform">Student Application</a></div>
+      <div><a href="https://forms.gle/t7kUsPtLZaNQL3Dq5">Instructors Application</a></div>
 
-    
+
+
+  </div>
+
+  <div class = "middle box">    
+      <div class="media-icons">
+
+          <a href="https://www.facebook.com/vietalkproject/"><i class="fab fa-facebook-f"></i></a>
+          <a href="https://www.instagram.com/vietalk_project/"><i class="fab fa-instagram"></i></a>
+          <a href="https://www.linkedin.com/company/vietalk-project/about/"><i class="fab fa-linkedin-in"></i></a>
       </div>
+  </div>
 
-      <div class = "middle box">    
-          <div class="media-icons">
+  <div class="right box">
+    <p>Copyright © 2022 <a href="#">VieTalk</a> All rights reserved</p>
 
-            <a href="https://www.facebook.com/vietalkproject/"><i class="fab fa-facebook-f"></i></a>
-            <a href="https://www.instagram.com/vietalk_project/"><i class="fab fa-instagram"></i></a>
-            <a href="https://www.linkedin.com/company/vietalk-project/about/"><i class="fab fa-linkedin-in"></i></a>
-          </div>
-      </div>
-  
-      <div class="right box">
-        <p>Copyright © 2022 <a href="#">VieTalk</a> All rights reserved</p>
 
-    
-
-    </div>
-    <div class=" bottom">
-     
-    </div>
-  </footer>
+</div>
+<div class=" bottom">
+ 
+</div>
+</footer>
 
 `);
